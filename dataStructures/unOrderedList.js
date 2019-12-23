44f4dd9cc3d94b56a3c4a89023396b2a10575930
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-23 11:43:46 +0530

    [TEJASWINI].Refactor programme  logic of service file changed to some extent

--- dataStructures/unOrderedList.js
+++ dataStructures/unOrderedList.js
@@ -1,3 +1,25 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node unOrderedList.js 
+ *                      2. if nodemon installed cmd> nodemon unOrdredList.js
+ * 
+ *  Purpose         :   Read the Text from a file, split it into words and arrange it as Linked List. 
+ *                      Take a user input to search a Word in the List. If the Word is not found then
+ *                      add it to the list, and if it found then remove the word from the List. 
+ *                      In the end save the list into a file.
+ * 
+ *  @description    
+ * 
+ *  @file           : unOrdredList.js
+ *  @overview       : Create a Unordered Linked List. The Basic Building Block is the Node Object. 
+ *                    Each node object must hold at least two pieces of information. 
+ *                    One ref to the data field and  second the ref to the next node object.
+
+ *  @module         : unorderedList - This is optional if expeclictly its an npm or local package
+ *  @author         : Tejaswini<chowdarytejaswini2@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 20-12-2019
+ *
+ ******************************************************************************/
 let readline = require("readline-sync");
 let util=require('../utility/dataStructureUtility'); //import the linkedlist.js file
 let fs = require("fs"); //built in library for read file
