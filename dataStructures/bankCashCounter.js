afb7f4c3a591f40588f03a65e00f6bf4dadea043
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-24 14:48:41 +0530

    [TEJASWINI].Refactor programe logic of service file changed to some extent

--- dataStructures/bankCashCounter.js
+++ dataStructures/bankCashCounter.js
@@ -1,3 +1,23 @@
+ /******************************************************************************
+ *  Execution       :   1. default node         cmd> node bankCashCounter.js 
+ *                      2. if nodemon installed cmd> nodemodule bankCashCounter.js
+ * 
+ *  Purpose         :    Create a Program which creates Banking Cash Counter where people come 
+ *                      in to deposit Cash and withdraw Cash. Have an input panel to add people to Queue to either 
+ *                      deposit or withdraw money and dequeue the people. Maintain the Cash Balance.
+
+ *  @description    
+ * 
+ *  @file           : bankCashCounter.js
+ *  @overview       :Write a Queue Class to enqueue and dequeue people to either
+ *                    deposit or withdraw money and maintain the cash balance.
+ *  @module         : bankCashCounter- This is optional if expeclictly its an npm or local package
+ *  @author         : Tejaswini<chowdarytejaswini2@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 23-12-2019
+ *
+ ******************************************************************************/
+ 
  let readline = require("readline-sync");
  let util = require('../utility/dataStructuresUtility');
  try{
