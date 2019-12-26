62ba2f6732f350b27306ebc3c3314e3029d603c7
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-26 15:32:55 +0530

    [Tejaswini].Refactor programme logic of service file changed to some extent

---
+++ tests_/addition.js
@@ -0,0 +1,22 @@
+let chai=require('chai');
+let expect=chai.expect;
+let addition=require("../functional/addition.js")
+describe('addition', ()=>{
+    it('it should add 2 nos', (done)=>{
+        addition.add(1,4).then(function(result){
+            expect(result).to.equal(5);
+            done();
+        }).catch(function(err){
+            done(err)
+        })
+    });
+    it('it should reject', (done)=>{
+        addition.add(1,4).catch(function(err){
+            expect(err).to.equal("x should be greater then 00");
+        })
+        .then(function(){done()})
+        .catch(function(err){
+            done(err);
+        })
+    })
+})
