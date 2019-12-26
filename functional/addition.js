8942189aad8321e2126707530e6f6ecdd445cb86
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-26 15:28:59 +0530

    [Tejaswini].Refactor programme logic of service file changed to some extent

--- functional/addition.js
+++ functional/addition.js
@@ -1,4 +1,18 @@
-
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node addition.js 
+ *                      2. if nodemon installed cmd> nodemon addition.js
+ * 
+ *  Purpose         : to find addition of  (x, y) and find the result.
+ *  @description    
+ * 
+ *  @file           : distance.js
+ *  @overview       : to find the ddition of two numbers.
+ *  @module         : distance - This is optional if expeclictly its an npm or local package
+ *  @author         : Tejaswini <chowdarytejaswini2@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 14-12-2019
+ *
+ ******************************************************************************/
 
 let readline=require('readline-sync');
 let util = require("../utility/functionalUtility");
