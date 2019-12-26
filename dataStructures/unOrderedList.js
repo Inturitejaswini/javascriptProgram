afbd7e52409873388696f1242d31e67137788e8a
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-26 12:51:58 +0530

    [Tejaswini].Refactor programme logic of service file changed to some extent

--- dataStructures/unOrderedList.js
+++ dataStructures/unOrderedList.js
@@ -22,9 +22,9 @@
  ******************************************************************************/
 let readline = require("readline-sync");
 let util=require('../utility/dataStructureUtility'); //import the linkedlist.js file
+try{
 let fs = require("fs"); //built in library for read file
 let data = fs.readFileSync('../../teju.txt'); //read the data from StringList.txt file
-try{
 console.log("String data read from file:\n" + data);
 stringArray = data.toString().toLocaleLowerCase().split(' '); //convert string data to array
 searchWord = readline.question("Enter a word to search : "); //user input to search element
