9d5264bf219c713049b2de7f085a0947491b7bce
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-28 18:17:56 +0530

    [Tejaswini].Refactor programme logic serviece file changed some extent

--- objectOrientedProgrammes/inventoryManagement.js
+++ objectOrientedProgrammes/inventoryManagement.js
@@ -14,33 +14,27 @@
  *  @module         : inventoryManagement - This is optional if expeclictly its an npm or local package
  *  @author         : Tejaswini<chowdarytejaswini2@gmail.com>
  *  @version        : 1.0
- *  @since          : 26-12-2019
+ *  @since          : 28-12-2019
  *
  ******************************************************************************/
 let fs=require('fs');
 let data=fs.readFileSync('./inventory.json');
 let obj = JSON.parse(data);
-let arr=[];
+let count=0;
+let count1=0;
+let count2=0;
 console.log('====================================');
 console.log("Resultant of Rice:");
 console.log('====================================');
 let len = obj.rice.length;
-//let price=0;
 for (let i = 0; i < len; i++) {
     let name = obj.rice[i].name;
     let weight = obj.rice[i].weight;
      let price =obj.rice[i].price;
-     arr[i]=price;
-    let company = (name, weight, price);
+    count=count+obj.rice[i].price;
     console.log("Name : " + name + " , " + "Weight : " + weight + " , " + "price : " + price);
-    console.log("Total price of rice is =" + company);
+    console.log("Total price of rice is =" + count);
 }
-let i=0,sum=0;
-while(i!=arr.length){
-    sum=sum+arr[i];
-    i++;
-}
-console.log("total sum of rice prices"+sum);
 console.log('====================================');
 console.log("Resultant of pulses:");
 console.log('====================================');
@@ -48,17 +42,10 @@ for (let i= 0; i < len; i++) {
     let name = obj.pulses[i].name;
     let weight = obj.pulses[i].weight;
     let price = obj.pulses[i].price;
-   
-    let company = (name, weight, price);
+   count1=count1+obj.pulses[i].price;
     console.log("Name : " + name + " , " + "Weight : " + weight + " , " + "price : " + price);
-    console.log("Total price of  is =" + company);
+    console.log("Total price of  is =" + count1);
 }
-let j=0,sum1=0;
-while(j!=arr.length){
-    sum1=sum1+arr[j];
-    j++;
-}
-console.log("total sum of pulces prices"+sum1);
 console.log('====================================');
 console.log("Resultant of wheat:");
 console.log('====================================');
@@ -66,14 +53,7 @@ for (let i = 0; i < len; i++) {
     let name = obj.wheat[i].name;
     let weight = obj.wheat[i].weight;
     let price = obj.wheat[i].price;
-    let company = (name, weight, price);
+    count2=count2+obj.wheat[i].price;
     console.log("Name : " + name + " , " + "Weight : " + weight + " , " + "price : " + price);
-    console.log("Total price of wheat is =" + company);
+    console.log("Total price of wheat is =" + count2);
 }
-let k=0,sum2=0;
-while(k!=arr.length){
-    sum2=sum2+arr[k];
-    k++;
-}
-console.log("total sum of wheat prices"+sum2);
-
---
+++ objectOrientedProgrammes/stock.js
@@ -0,0 +1,34 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node stock.js 
+ *                      2. if nodemon installed cmd> nodemodule stock.js
+ * 
+ *  Purpose         : Write a program to read in Stock Names, Number of Share, Share Price.
+ *                   Print a Stock Report with total value of each Stock and the total value of Stock.
+ *  @description    
+ * 
+ *  @file           :stock.js
+ *  @overview       :Print the Stock Report.
+ *  @module         :stock - This is optional if expeclictly its an npm or local package
+ *  @author         : Tejaswini<chowdarytejaswini2@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 28-12-2019
+ *
+ ******************************************************************************/
+let readline=require('readline-sync');
+let  fs = require('fs');
+let data = fs.readFileSync('./stock.json');
+let obj = JSON.parse(data);
+console.log('====================================');
+console.log("Resultant of Stock report");
+console.log('====================================');
+let len = obj.stock.length;
+let count=0;
+for (let i = 0; i < len; i++) {
+    let stockName = obj.stock[i].stockName;
+    let noOfShares = obj.stock[i].noOfShares;
+    let sharePrice = obj.stock[i].sharePrice;
+    count=count+obj.stock[i].sharePrice;
+    console.log("Name:"+stockName+","+"number:"+noOfShares+","+"price:"+sharePrice);
+    console.log("Total price of shares =" + sharePrice*noOfShares);
+    console.log("print the total share count"+count);
+}
