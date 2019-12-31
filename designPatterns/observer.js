c10225729d058a792d98dfb1541fd0168bb3c58c
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-31 17:48:28 +0530

    [Tejaswini].Refactor observer designpattern  business logic serviece file changed some extent

--- designPatterns/observer.js
+++ designPatterns/observer.js
@@ -1,3 +1,19 @@
+
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node observer.js 
+ *                      2. if nodemon installed cmd> nodemon observer.js
+ *  Purpose         : In observer pattern, the object that watch on the state of another object 
+ *                   are called Observer and the object that is being watched is called Subject. 
+ *  @description    
+ *  @file           : observer.js
+ *  @overview       :  Whenever any new message will be posted to the topic, all the registers 
+ *                     observers will be notified and they can consume the message.
+ *  @module         : observer- This is optional if expeclictly its an npm or local package
+ *  @author         : Tejaswini <chowdarytejaswini2@gmail.com>
+ *  @version        : 1.0
+ *  @since          :31-12-2019
+ *
+ ******************************************************************************/
 class Observable {
     constructor() 
     {
