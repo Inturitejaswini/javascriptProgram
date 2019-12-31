de75fcd500c31cee64f7c5b3fc1fe3dae649f605
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-31 18:48:20 +0530

    [Tejaswini].Refactor factory designpattern  business logic serviece file changed some extent

--- designPatterns/factory.js
+++ designPatterns/factory.js
@@ -14,32 +14,11 @@
  *  @since          :31-12-2019
  *
  ******************************************************************************/
-class Computer {
-    constructor(ram,hdd,processor) 
-    {
-      this.ram=ram;
-      this.processor=processor;
-      this.hdd=hdd;
-    }
+let readline = require("readline-sync");
+let util=require('../utility/designPatternsUtility');
+try{
+    util.factoryDesign();
 }
-let factory=(type,ram,hdd,processor) =>{
-    this.type=type;
-    if(type=="pc"){
-        let pc=new Computer(ram,hdd,processor);
-        console.log(type +":");
-        console.log(pc.ram+"ram"+pc.hdd+"hdd"+pc.processor+":processor");
-    }
-    if(type=="laptop"){
-        let laptop=new Computer(ram,hdd,processor);
-        console.log(type+":");
-        console.log(laptop.ram+"ram,"+laptop.hdd+"hdd,"+laptop.processor+":processor");
-    }
-    if(type=="server"){
-        let server=new Computer(ram,hdd,processor);
-        console.log(type+":");
-        console.log(server.ram+"ram,"+server.hdd+"hdd,"+server.processor+":processor");
-    }
+catch(e){
+    console.log(e);
 }
-factory("server","2 gb","500 gb","i7");
-factory("pc","2 gb"," 500 gb"," i7");
-factory("laptop","4 gb","1 tb","i11");
---
+++ utility/designPatternsUtility.js
@@ -0,0 +1,32 @@
+
+exports.factoryDesign=()=>{
+class Computer {
+    constructor(ram,hdd,processor) 
+    {
+      this.ram=ram;
+      this.processor=processor;
+      this.hdd=hdd;
+    }
+}
+let factory=(type,ram,hdd,processor) =>{
+    this.type=type;
+    if(type=="pc"){
+        let pc=new Computer(ram,hdd,processor);
+        console.log(type +":");
+        console.log(pc.ram+"ram"+pc.hdd+"hdd"+pc.processor+":processor");
+    }
+    if(type=="laptop"){
+        let laptop=new Computer(ram,hdd,processor);
+        console.log(type+":");
+        console.log(laptop.ram+"ram,"+laptop.hdd+"hdd,"+laptop.processor+":processor");
+    }
+    if(type=="server"){
+        let server=new Computer(ram,hdd,processor);
+        console.log(type+":");
+        console.log(server.ram+"ram,"+server.hdd+"hdd,"+server.processor+":processor");
+    }
+}
+factory("server","2 gb","500 gb","i7");
+factory("pc","2 gb"," 500 gb"," i7");
+factory("laptop","4 gb","1 tb","i11");
+}
