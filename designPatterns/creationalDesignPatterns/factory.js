9a37d504e650e2353b53a59c1709cd7b2913a2f4
Inturitejaswini <tejaswinichowdary113@gmail.com>
2019-12-31 15:57:31 +0530

    [Tejaswini].Refactor factory designpattern  business logic serviece file changed some extent

--- designPatterns/creationalDesignPatterns/factory.js
+++ designPatterns/creationalDesignPatterns/factory.js
@@ -30,11 +30,17 @@ let factory=(type,ram,hdd,processor) =>{
         console.log(type +":");
         console.log(pc.ram+"ram"+pc.hdd+"hdd"+pc.processor+":processor");
     }
-    if(type=="server"){
-        let pc=new Computer(ram,hdd,processor);
+    if(type=="laptop"){
+        let laptop=new Computer(ram,hdd,processor);
         console.log(type+":");
-        console.log(pc.ram+"ram,"+pc.hdd+"hdd,"+pc.processor+":processor,");
+        console.log(laptop.ram+"ram,"+laptop.hdd+"hdd,"+laptop.processor+":processor");
+    }
+    if(type=="server"){
+        let server=new Computer(ram,hdd,processor);
+        console.log(type+":");
+        console.log(server.ram+"ram,"+server.hdd+"hdd,"+server.processor+":processor");
     }
 }
 factory("server","2 gb","500 gb","i7");
 factory("pc","2 gb"," 500 gb"," i7");
+factory("laptop","4 gb","1 tb","i11");
