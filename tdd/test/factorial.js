c6863293afdba9dd78116a690240d7ce5c8657c4
Inturitejaswini <tejaswinichowdary113@gmail.com>
2020-01-02 18:25:48 +0530

    [Tejaswini].Refactor tdd testing factorial  business logic changed some extent

--- tdd/test/factorial.js
+++ tdd/test/factorial.js
@@ -1,11 +1,11 @@
 /******************************************************************************
- *  Execution       :   1. default node         cmd> node windChill.js 
- *                      2. if nodemon installed cmd> nodemon windChill.js
- *  Purpose         :teting the windChill programme using tdd.
+ *  Execution       :   1. default node         cmd> node factorial.js 
+ *                      2. if nodemon installed cmd> nodemon factorial.js
+ *  Purpose         :teting the factorial programme using tdd.
  *  @description   
- *  @file           :windChill.js
+ *  @file           :factorial.js
  *  @overview       : using tdd testing test the programme.
- *  @module         : windChill- This is optional if expeclictly its an npm or local package
+ *  @module         : factorial- This is optional if expeclictly its an npm or local package
  *  @author         : Tejaswini <chowdarytejaswini2@gmail.com>
  *  @version        : 1.0
  *  @since          : 2-01-2020
