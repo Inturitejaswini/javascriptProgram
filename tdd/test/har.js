654fec46a731527955f41282bf4fc78d34fe5c2e
Inturitejaswini <tejaswinichowdary113@gmail.com>
2020-01-02 17:53:41 +0530

    [Tejaswini].Refactor tdd testing harmonic  business logic changed some extent

--- tdd/test/har.js
+++ tdd/test/har.js
@@ -1,3 +1,16 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node harmonic.js 
+ *                      2. if nodemon installed cmd> nodemon harmonic.js
+ *  Purpose         :teting the harmonic programme using tdd.
+ *  @description   
+ *  @file           : harmonic.js
+ *  @overview       : using tdd testing test the programme.
+ *  @module         : har- This is optional if expeclictly its an npm or local package
+ *  @author         : Tejaswini <chowdarytejaswini2@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 2-01-2020
+ *
+ ******************************************************************************/
 let chai = require('chai')
 let expect = chai.expect
 
